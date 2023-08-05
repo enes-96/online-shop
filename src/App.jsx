f76e1174c17dfafd8c07a3aff95f9cd2953ff620
enes-96 <seyit7enes@gmail.com>
2023-08-05 20:15:44 +0200

    working on react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { fetchProducts } from "./utils/api";
+import React from "react";
+import Header from "./components/common/Header";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  const fetchProductData = async () => {
-    try {
-      const data = await fetchProducts();
-      setProducts(data);
-    } catch (error) {
-      console.error("Error fetching products:", error);
-    }
-  };
-
-  // useEffect(() => {
-  // fetchProductData();
-  // }, []);
 
   return (
     <div>
-      <header>
-        <h1 className="text-3xl">Product List</h1>
-      </header>
-      <main>
-        <ul>
-          {products.map((product) => (
-            <li key={product.id}>
-              <div>
-                <p>{product.title}</p>
-                <img src={product.image} alt={product.title} />
-              </div>
-            </li>
-          ))}
-        </ul>
-      </main>
+      <Header />
     </div>
   );
 }
---
+++ src/components/common/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <p>hi from header </p>
+            </div>
+        </div>
+    )
+}
+export default Header
