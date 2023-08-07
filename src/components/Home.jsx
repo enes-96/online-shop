f2aafa992f27652c907e57b42b4bb577a165c485
enes-96 <seyit7enes@gmail.com>
2023-08-07 23:34:33 +0200

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
     return (
-        <div>
-            <h1 className="text-9xl">Random clothes you need!</h1>
-            <Link className="text-4xl bg-slate-300 py-1 px-4" to="/products">Buy them!  </Link>
+        <div id="heroBg" className="h-screen gird ">
+            <h1 className="text-7xl text-center p-12 text-white">Random clothes you need!</h1>
+            <div className="flex justify-center p-12">
+                <Link className="text-4xl bg-slate-300 py-1 px-4" to="/products">Buy them!  </Link>
+            </div>
         </div>
     )
 }
